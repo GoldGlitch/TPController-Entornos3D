bd1ac27a108e276d035bbf7ee431b5d14c8129e0
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-03-21 00:15:23 +0100

    V1
    escenario, primer IA y player version 1 completados

---
+++ TPController/Assets/Personaje/Scripts/InputData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct InputData
+{
+    //movimiento
+    public float Horizontal;
+    public float Vertical;
+
+    //mouse rotacion
+    public float verticalMouse;
+    public float horizontalMouse;
+
+    //mov extras
+    public bool dash;
+    public bool jump;
+
+    public void getInput()
+    {
+        //movement
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        //mouse rotation
+        verticalMouse = Input.GetAxis("Mouse Y");
+        horizontalMouse = Input.GetAxis("Mouse X");
+
+        //Extra movemnts
+        dash = Input.GetButton("Dash");
+        jump = Input.GetButtonDown("Jump");
+    }
+}
---
+++ TPController/Assets/Personaje/Scripts/_characterController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class _characterController : MonoBehaviour
+{
+    public Camera cam;
+    private InputData input;
+    private AnimMovement characterMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterMovement = GetComponent<AnimMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from player
+        input.getInput();
+
+        //Apply input to character
+        CharacterMovement();
+    }
+  
+    void CharacterMovement()
+    {
+        characterMovement.moveCharacter(input.Horizontal, input.Vertical, cam, input.jump, input.dash);
+
+    }
+
+
+}
